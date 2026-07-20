4e8b66430fd041d96cd8d0f760bcb116a199275a
betsegaw <betsegawlemma@gmail.com>
2026-07-20 15:47:52 +0300

    Revert "Merge pull request #31 from HABTec/feat/hide-inteligence-for-non-moh-user"
    This reverts commit 9ceb413252404d4db7a40c8ca04c12e9b02603c4, reversing
    changes made to 2f70e2ef59309c381bf39db7600b8243b1994082.

--- superset/landing_view.py
+++ superset/landing_view.py
@@ -168,11 +168,5 @@ class MoHLandingView(IndexView):
             featured=featured,
             others=others,
             is_admin=security_manager.is_admin(),
-            health_intelligence_url=(
-                current_app.config.get("MOH_HEALTH_INTELLIGENCE_URL")
-                if security_manager.can_access_moh_dashboard(
-                    next((dashboard for dashboard in rows if dashboard.id == 8), None)
-                )
-                else None
-            ),
+            health_intelligence_url=current_app.config.get("MOH_HEALTH_INTELLIGENCE_URL"),
         )
